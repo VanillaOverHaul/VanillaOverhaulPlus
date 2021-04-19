0a11b90c59db5ffd28f057f01eeeba56e8b8bff9
PheonixVX <TheGrimReaper.VX@gmail.com>
2021-04-18 21:03:26 -0400

    Base

---
+++ src/main/java/io/github/vanillaoverhaulplus/client/VanillaOverhaulPlusClient.java
@@ -0,0 +1,13 @@
+package io.github.vanillaoverhaulplus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class VanillaOverhaulPlusClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient () {
+
+	}
+}
